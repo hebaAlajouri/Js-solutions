5f05ae7caca38983f2c2b6a2832791281893ff50
HebaAlajouri <hebaajoury1212@gmail.com>
2025-02-01 13:26:39 +0300

    Create q1+q3.js

---
+++ task1/loops/q1+q3.js
@@ -0,0 +1,40 @@
+"use strict";
+for(let i=1;i<51;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+    else{
+        continue;
+    }
+}
+let i=1;
+while(i<51){
+    if(i%2==0){
+        console.log(i);
+        i++;
+    }
+    else{
+        i++;
+        continue;
+    }
+}
+/////////////////////q3//////////////
+for(let i=1;i<51;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+    else{
+        continue;
+    }
+}
+ i=1;
+while(i<51){
+    if(i%2==1){
+        console.log(i);
+        i++;
+    }
+    else{
+        i++;
+        continue;
+    }
+}
