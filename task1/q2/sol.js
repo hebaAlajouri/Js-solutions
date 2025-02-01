432471c2f50a86d0b8683be7edad4ba320f43aad
HebaAlajouri <hebaajoury1212@gmail.com>
2025-02-01 13:25:00 +0300

    Create sol.js

---
+++ task1/q2/sol.js
@@ -0,0 +1,21 @@
+"use strict";
+console.log(typeof(100));//number
+console.log(typeof(73.9));//number
+console.log(typeof(NaN));//number
+console.log(typeof("water"));//string
+console.log(typeof(false));//boolean
+console.log(typeof(9 != 11));//boolean
+console.log("Orang"+"e");//Orange
+console.log("Orange"-"s");//NaN
+console.log("4"+"8");//48
+console.log("4"-"8");//-4
+console.log("name"+"3");//name3
+console.log("name"-"3");//NaN
+console.log(82*"word");//NaN
+console.log(1+"hello");//1hello
+console.log("hello"+1);//hello1
+console.log(1+true);//2
+console.log("hello"+true);//hellotrue
+console.log(typeof(Infinity));//number
+console.log(1=='1');//true
+console.log(1==='1');//false
