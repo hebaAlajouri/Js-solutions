339b90470005d9f827cf3fc05913cc37e8a6b974
HebaAlajouri <hebaajoury1212@gmail.com>
2025-02-01 13:27:18 +0300

    Create q2.js

---
+++ task1/Arrays/q2.js
@@ -0,0 +1,38 @@
+"use strict";
+var fruit = ["banana", "apple", "orange", "watermelon"]; 
+var vegetables = ["carrot", "tomato", "pepper", "lettuce"]; 
+//a
+// vegetables.pop();
+//////////////////////////////
+//b
+// fruit.unshift();
+//////////////////////////////
+//c
+// fruit.indexOf("orange");
+//////////////////////////////
+//d
+// fruit.push((fruit.indexOf("orange")).toString());
+// console.log(fruit);
+//////////////////////////////
+//e
+// vegetables.length();
+//////////////////////////////
+//f
+// vegetables.push((vegetables.length).toString());
+// console.log(vegetables);
+///////////////////////////////
+//g
+// let food=fruit.concat(vegetables);
+// console.log(food);
+////////////////////////////
+//h
+// food.pop();
+// food.pop();
+/////////////////////////////
+//i
+// food.reverse();
+/////////////////////////////
+//j
+// food.toString();
+/////////////////////////
+
