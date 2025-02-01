7aecd38b369a0badcba6076a7cd2c6db01e25537
HebaAlajouri <hebaajoury1212@gmail.com>
2025-02-01 13:26:34 +0300

    Create q10.js

---
+++ task1/loops/q10.js
@@ -0,0 +1,10 @@
+"use strict";
+let myString="CodingAcademy";
+let myArray=[7,500,"KH404","black",36];
+for(let i=0;i<myArray.length;i++){
+    console.log(myArray[i]+"\n");
+}
+for(let i=myString.length-1;i>=0;i--){
+    console.log(myString.charAt(i)+"\n");
+    
+}
